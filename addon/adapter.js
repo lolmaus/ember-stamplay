afececfd6c34ea1e3a4a530e6a38517c567449ce
Andrey Mikhaylov (lolmaus) <lolmaus@gmail.com>
2016-04-03 22:39:30 +0300

    Depagination support for findAll (forced) and query (optional via allPages: true)

--- addon/adapter.js
+++ addon/adapter.js
@@ -1,14 +1,22 @@
+/* jshint ignore:start */
 import Ember from 'ember';
 
 const {
   computed: {alias},
+  copy,
   inject:   {service},
+  RSVP,
   String:   {underscore}
 } = Ember;
 
 import RESTAdapter from 'ember-data/adapters/rest';
 
 export default RESTAdapter.extend({
+
+  // ----- Arguments -----
+  PER_PAGE: 20,
+
+
 
   // ----- Services -----
   ajaxStamplay: service(),
@@ -17,6 +25,17 @@ export default RESTAdapter.extend({
 
   // ----- Overridden properties -----
   host: alias('ajaxStamplay.host'),
+
+
+
+  // ----- Overridden methods -----
+  findAll(store, ModelClass/*, sinceToken, snapshotRecordArray*/) {
+    const per_page = this.get('PER_PAGE');
+
+    return this
+      .query(store, ModelClass, {per_page})
+      .then(response => this.loadAllPages({response, ModelClass, store, requestType: 'findAll'}));
+  },
 
   pathForType (modelName) {
     const finalModelName = underscore(modelName);
@@ -27,5 +46,69 @@ export default RESTAdapter.extend({
 
     const objectPath = this.get('ajaxStamplay.objectPath');
     return `${objectPath}/${finalModelName}`;
+  },
+
+  query(store, ModelClass, query = {}) {
+    query = copy(query);
+
+    const allPages = query.allPages;
+    delete query.allPages;
+
+    if (!query.per_page) {
+      query.per_page = this.get('PER_PAGE');
+    }
+
+    const firstPagePromise = this._super(store, ModelClass, query);
+
+    if (!allPages) {
+      return firstPagePromise;
+    }
+
+    return firstPagePromise
+      .then(response => this.loadAllPages({response, ModelClass, store, requestType: 'query'}));
+  },
+
+
+
+  // ----- Custom methods -----
+  loadAllPages ({response: firstPage, ModelClass, store, query = {}, requestType}) {
+    const totalPages =
+      firstPage
+      && firstPage.pagination
+      && firstPage.pagination.total_pages;
+
+    if (!totalPages || totalPages < 2) {
+      return firstPage;
+    }
+
+    const otherPageRequests = this.produceOtherPageReqeusts({totalPages, ModelClass, store, query});
+
+    return RSVP
+      .all([firstPage, ...otherPageRequests])
+      .then(responses => this.reducePaginatedResponses(responses));
+  },
+
+  produceOtherPageReqeusts ({totalPages, ModelClass, store, query = {}}) {
+    query = copy(query);
+
+    if (!query.per_page) {
+      query.per_page = this.get('PER_PAGE');
+    }
+
+    return Array
+      .apply(null, Array(totalPages - 1)) // We start with the second page
+      .map((_, i) => {
+        const page = i + 2; // Stamplay pages are one-indexed
+        return this.query(store, ModelClass, {...query, page});
+      });
+  },
+
+  reducePaginatedResponses (responses) {
+    const data =
+      responses
+        .map(response => response.data)
+        .reduce((result, response) => result.concat(response), []);
+
+    return {data};
   }
 });
--- addon/serializer.js
+++ addon/serializer.js
@@ -8,7 +8,21 @@ import JSONSerializer from 'ember-data/serializers/json';
 
 export default JSONSerializer.extend({
 
-  // ----- Overridden methods -----  
+  // ----- Overridden methods -----
+  // extractMeta: function(store, typeClass, payload) {
+  //   if (payload && payload.pagination) {
+  //     store.setMetadataFor(typeClass, payload.pagination);
+  //     delete payload.pagination;
+  //   }
+  // },
+
+  normalizeArrayResponse (store, primaryModelClass, payload, id, requestType) {
+    const arrayPayload = payload.data.slice();
+    arrayPayload.meta  = payload.pagination;
+
+    return this._super(store, primaryModelClass, arrayPayload, id, requestType);
+  },
+
   serializeIntoHash(hash, typeClass, snapshot, options) {
     const serializedRecord = this.serialize(snapshot, options);
     merge(hash, serializedRecord);
